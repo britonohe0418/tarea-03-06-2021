deaf8d8f31d3c3dc42cad3a53f9c555a316c5936
britonohe0418 <anonymous>
2021-06-11 23:37:08 -0500

    ej1 metodo mostrar clase fraccion

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -2,3 +2,6 @@ class Fraccion:
     def __init__(self, numerador, denominador):
         self.num = numerador
         self.den = denominador
+
+    def mostrar(self):
+        return '* {}/{}.'.format(self.num, self.den)
